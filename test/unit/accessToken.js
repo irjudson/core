dec34e24f12440e6e14c7022d810261fcb0f4043
Tim Park <timfpark@gmail.com>
2015-02-02 14:23:52 -0800

    check to make sure token is valid

--- test/unit/accessToken.js
+++ test/unit/accessToken.js
@@ -1,6 +1,7 @@
 var assert = require('assert')
   , moment = require('moment')
-  , core = require('../../lib');
+  , core = require('../../lib')
+  , jwt = require('jsonwebtoken');
 
 describe('accessToken service', function() {
     it('can create and remove tokens', function(done) {
@@ -29,15 +30,23 @@ describe('accessToken service', function() {
             });
         });
     });
+
     it('can create a token with the correct default expiration (1 day)', function(done) {
         core.services.accessTokens.create(core.fixtures.models.principals.anotherUser, function(err, accessToken) {
             assert(!err);
             var expires = moment(accessToken.expiration);
             var oneDayFromNow = moment().add(1, 'days');
             assert((oneDayFromNow - expires) == (60 * 60 * 24 * 1000));
-            done();
-        });    
+
+            // test to make sure token is valid.
+            jwt.verify(accessToken.token, core.config.access_token_signing_key, function(err, jwtToken) {
+                assert(!err);
+
+                done();
+            });
+        });
     });
+
     it('can create a token with a custom expiration', function(done) {
         core.services.accessTokens.create(core.fixtures.models.principals.anotherUser, { expires: moment().add(30, 'days') }, function(err, accessToken) {
             assert(!err);
@@ -45,6 +54,6 @@ describe('accessToken service', function() {
             var thirtyDaysFromNow = moment().add(30, 'days');
             assert((thirtyDaysFromNow - expires) == (30 * 60 * 60 * 24 * 1000));
             done();
-        });    
+        });
     });
 });
